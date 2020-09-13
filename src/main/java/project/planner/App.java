e471e1a6da8b7986c973740407273711522bb05b
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-09-13 17:02:05 -0400

    Planning out the app via services

--- src/main/java/project/planner/App.java
+++ src/main/java/project/planner/App.java
@@ -3,5 +3,10 @@ package project.planner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Start App");
+        /*
+        Create a Project class
+        Service to read Project and place into an Array
+        Service to take Array and make an Excel sheet
+         */
     }
 }
