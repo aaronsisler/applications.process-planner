65b4ae47a19147a16621c6f45008a8f1cf308aef
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-10-11 14:10:08 -0400

    Adding in blank folder reader service with shell methods

---
+++ src/main/java/process/planner/utils/FolderReader.java
@@ -0,0 +1,17 @@
+package process.planner.utils;
+
+import java.util.ArrayList;
+
+public class FolderReader {
+    public ArrayList<String> retrieveDateFiles() {
+        ArrayList<String> dateFileList = new ArrayList<>();
+
+        return dateFileList;
+    }
+
+    public ArrayList<String> retrieveProcessDefinitionFiles() {
+        ArrayList<String> processDefinitionFileList = new ArrayList<>();
+
+        return processDefinitionFileList;
+    }
+}
